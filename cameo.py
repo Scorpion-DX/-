65c2f948cf4b91e82fdf3333df771ad3b3057679
Scorpion-DX <42525548+Scorpion-DX@users.noreply.github.com>
2018-08-20 13:25:59 +0800

    Update cameo.py

--- cameo.py
+++ cameo.py
@@ -1,43 +1,36 @@
 import cv2
-from managers import WindowManager, CaptureManager
 import filters
-
-
+from managers import WindowManager,CaptureManager
+import os
+os.chdir(r'F:\Python练习\Opencv练习\OK')
 class Cameo(object):
     def __init__(self):
-        self._windowManger = WindowManager('Cameo', self.onKeypress)
-        self._captureManger = CaptureManager(cv2.VideoCapture(0), self._windowManger, True)
-        #self._curveFilter=filters.Blur()
-
+        self._windowManager=WindowManager('Cameo',self.onKeypress)
+        self._captureManager=CaptureManager(cv2.VideoCapture(0),self._windowManager,True)
+        self._curveFilter=filters.BlurFilter()
     def run(self):
-        """Run the main loop"""
-        self._windowManger.createdWindow()
-        while self._windowManger.isWindowCreated:
-            self._captureManger.enterFrame()
-            frame = self._captureManger.frame
-
-            #filters.strokeEdges(frame,frame)
-            #self._curveFilter.apply(frame,frame)
-
-            self._captureManger.exitFrame()
-            self._windowManger.processEvents()
-
-    def onKeypress(self, keycode):
-        """Handle a keypress.
-        space   -> Take a screenshot
-        tab     -> Start/stop recoding a screenshot
-        escape  -> Quit
-        """
-        if keycode == 32:  # space
-            self._captureManger.writeImage("./screenshot.png")
-        elif keycode == 9:  # tab
-            if not self._captureManger.isWritingVideo:
-                self._captureManger.startWritingVideo('./screenshot.avi')
+        self._windowManager.createWindow()
+        while self._windowManager.isWindowCreated:
+            self._captureManager.enterFrame()
+            frame=self._captureManager.frame
+            #add additional function here
+            filters.strokeEdges(frame,frame)
+            self._curveFilter.apply(frame,frame)
+            
+            self._captureManager.exitFrame()
+            self._windowManager.processEvents()
+        self._captureManager._capture.release()
+    def onKeypress(self,keycode):
+        if keycode==32:#space
+            self._captureManager.writeImage('./data/pictures/screenshot.png')
+        elif keycode==9: #tab
+            if not self._captureManager.isWritingVideo:
+                self._captureManager.startWritingVideo(r'F:/screen_cast.avi')
             else:
-                self._captureManger.stopWritingVideo()
-        elif keycode == 27:  # escape
-            self._windowManger.destroyWindow()
-
-
-if __name__ == '__main__':
+                self._captureManager.stopWritingVideo()
+        elif keycode==27: #exit
+            self._windowManager.destroyWindow()
+if __name__=='__main__':
     Cameo().run()
+    
+
